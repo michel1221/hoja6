1b7292fb023ab8591c6d3f66452c19a304e00dbf
michel <michel@michel-PC>
2015-08-18 23:55:01 -0500

    primera correcion HT4

---
+++ Hoja de trabajo 4/src/Calculadora.java
@@ -0,0 +1,14 @@
+public interface Calculadora{
+
+	public void setString(String linea);
+	
+	public void meterVector ();
+	
+	public boolean calcularVector();
+	
+	public int getResultado ();
+	
+
+
+
+}
