0b03b9764bb5bd0fa40200e7228a1329561b7d1c
michel <michel@172.20.0.154>
2015-08-19 21:55:16 -0500

    diecisieteabo correcion HT4

--- Hoja de trabajo 4/src/StackVector.java
+++ Hoja de trabajo 4/src/StackVector.java
@@ -4,41 +4,65 @@
 // Luis Eduardo Ruano
 // Luis pedro Velezques
 import java.util.*;
-public class StackVector <E> implements Stack <E>{
 
-	public Vector <E>dato;
+/**
+ *
+  * @author michel Ramirez
+ * @author Luis Eduardo
+ * @author Luis Pedro
+ * @param <E>
+ */
+public class StackVector <E> extends abstractStack <E>{
 
-	public StackVector (){
+    /**
+     * sirve para poder crear un objeto de tipo vector
+     */
+    public Vector <E>dato;
+
+    /**
+     *constructor
+     */
+    public StackVector (){
 	
 	dato= new Vector<E>();
 	
 	}
 
-	public void push(E item){
+    /**
+     *sirve para poder meter un valor al stackg
+     * @param item
+     */
+    public void push(E item){
 	 
 		dato.add(item);
 	}
-	public E pop(){
+
+    /**
+     *sirve para sacar un valor del stacj
+     * @return
+     */
+    public E pop(){
 	   
 	   return dato.remove(size()-1);
 	}
 
-	public E peek(){
+    /**
+     *sirve para agarrar el valor del stack
+     * @return
+     */
+    public E peek(){
 	
 		return dato.get(size() - 1);
 	}
 
-   
-   public boolean empty(){
-	     
-		return size()==0;
-   }
-   
-	public int size(){
+    /**
+     * sirve para poder tomar el tamaño
+     * @return
+     */
+    public int size(){
 	
 	   return dato.size();
 	}
-
 
 
 
