98427e914582267e6957a6562b01ef7b652d2d26
michel <michel@172.20.0.154>
2015-08-19 21:52:54 -0500

    quinceaba correcion HT4

--- Hoja de trabajo 4/src/Stack.java
+++ Hoja de trabajo 4/src/Stack.java
@@ -1,23 +1,51 @@
+
+/**
+ *
+ * @author michel Ramirez
+ * @author Luis Eduardo
+ * @author Luis Pedro
+ * @param <E>
+ */
 public interface Stack<E> 
 {
 
-   public void push(E item);
+    /**
+     *sirve para poder  meter un valor
+     * @param item
+     */
+    public void push(E item);
    // pre: 
    // post: item is added to stack
    // will be popped next if no intervening push
    
-   public E pop();
+    /**
+     * sirve para poder sacar un valor 
+     * @return
+     */
+    public E pop();
    // pre: stack is not empty
    // post: most recently pushed item is removed and returned
    
-   public E peek();
+    /**
+     * sirve para poder agarrar un valor del stack
+     * @return
+     */
+    public E peek();
    // pre: stack is not empty
    // post: top value (next to be popped) is returned
    
-   public boolean empty();
+    /**
+     * sirve para pdoer vaciar el experimento
+     * @return
+     */
+    public boolean empty();
    // post: returns true if and only if the stack is empty
    
-   public int size();
+    /**
+     *sirve para poder tener el valor del stack
+     * @return
+     */
+    public int size();
    // post: returns the number of elements in the stack
 
 }
